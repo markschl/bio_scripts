bc43ff89e22b7e564ab57ca5b7a25b4f28feae62
markschl <markschl19@gmail.com>
2018-07-19 20:03:33 +0200

    Restructure extract_taxonomy

--- py/extract_taxonomy.py
+++ py/extract_taxonomy.py
@@ -14,64 +14,63 @@ def extract(taxonomy, output, ranks='', illegal=' ', unknown=None):
         unknown = re.compile(r'({})'.format('|'.join(re.escape(u) for u in unknown.split(','))), re.I)
     order = RankOrder()
 
-    with open(taxonomy) as t:
-        ranks = list(reversed(ranks))
-        for row in csv.reader(t, delimiter='\t'):
-            id = row[0]
-            lineage = [n.split(':', 1) for n in reversed(row[1:])]
-            lineage_dict = {r[0]: (r[1], i) for i, r in enumerate(lineage)}
-            l = len(lineage)
-            out = []
-            # ranks are added in reverse order (from lowest to highest)
-            # position within input lineage
-            i = 0
-            # position of next taxon that has no 'unknown' keyword
-            known_i = None
-            for r, rank in enumerate(ranks):
-                if rank not in lineage_dict:
-                    # rank not present -> try to infer the best parent rank based on the rank name
-                    for j, x in enumerate(lineage[i+1:]):
-                        rank2, name2 = x
-                        cmp = order.cmp_names(rank, rank2)
-                        if cmp is not None:
-                            # matching rank found -> set position depending on relationship
-                            i += j + cmp
-                    if i + 1 >= l:
-                        name = 'unknown'
-                    else:
-                        rank2, name2 = lineage[i + 1]
-                        name = '{}_{}'.format(name2, rank2)
+    ranks = list(reversed(ranks))
+    for row in csv.reader(taxonomy, delimiter='\t'):
+        id = row[0]
+        lineage = [n.split(':', 1) for n in reversed(row[1:])]
+        lineage_dict = {r[0]: (r[1], i) for i, r in enumerate(lineage)}
+        l = len(lineage)
+        out = []
+        # ranks are added in reverse order (from lowest to highest)
+        # position within input lineage
+        i = 0
+        # position of next taxon that has no 'unknown' keyword
+        known_i = None
+        for r, rank in enumerate(ranks):
+            if rank not in lineage_dict:
+                # rank not present -> try to infer the best parent rank based on the rank name
+                for j, x in enumerate(lineage[i+1:]):
+                    rank2, name2 = x
+                    cmp = order.cmp_names(rank, rank2)
+                    if cmp is not None:
+                        # matching rank found -> set position depending on relationship
+                        i += j + cmp
+                if i + 1 >= l:
+                    name = 'unknown'
                 else:
-                    name, i = lineage_dict[rank]
+                    rank2, name2 = lineage[i + 1]
+                    name = '{}_{}'.format(name2, rank2)
+            else:
+                name, i = lineage_dict[rank]
 
-                if unknown is not None:
-                    # in case there are unknown names, proceed to higher ranks
-                    if known_i is not None and i >= known_i:
-                        # all ranks up to this one were unknown, this one is known
-                        known_i = None
-                    elif known_i is not None:
-                        # current rank unknown (known_i is higher than i)
-                        name = '{}_unknown'.format(lineage[known_i][1])
-                    else:
-                        # check if unknown
-                        j = i
-                        while unknown.search(name) is not None:
-                            j += 1
-                            known_i = j
-                            if j >= l:
-                                name = 'unknown'
-                                known_i = None
-                                break
-                            name = lineage[known_i][1]
+            if unknown is not None:
+                # in case there are unknown names, proceed to higher ranks
+                if known_i is not None and i >= known_i:
+                    # all ranks up to this one were unknown, this one is known
+                    known_i = None
+                elif known_i is not None:
+                    # current rank unknown (known_i is higher than i)
+                    name = '{}_unknown'.format(lineage[known_i][1])
+                else:
+                    # check if unknown
+                    j = i
+                    while unknown.search(name) is not None:
+                        j += 1
+                        known_i = j
+                        if j >= l:
+                            name = 'unknown'
+                            known_i = None
+                            break
+                        name = lineage[known_i][1]
 
-                        if known_i is not None:
-                            name = '{}_unknown'.format(name)
+                    if known_i is not None:
+                        name = '{}_unknown'.format(name)
 
-                if illegal:
-                   name = illegal_patt.sub('_', name)
-                out.append(name)
-            out.append(id)
-            writer.writerow(reversed(out))
+            if illegal:
+               name = illegal_patt.sub('_', name)
+            out.append(name)
+        out.append(id)
+        writer.writerow(reversed(out))
 
 
 # species group / subgroup are not recognized separately
@@ -136,7 +135,7 @@ Names containing one of the '--unknown' keywords will be replaced by the next de
 upwards in the hierarchy, and '_unknown' is appended.
 Make sure to always specify the ranks in the correct order, otherwise there will be confusion.
 """)
-p.add_argument("taxonomy", help="File with Genbank accession on each line")
+p.add_argument("taxonomy", type=argparse.FileType('r'), help="File with Genbank accession on each line")
 p.add_argument("-r", "--ranks", help="comma delimited list of ranks to extract",
                default="superkingdom,kingdom,phylum,class,order,family,genus,species,varietas")
 p.add_argument("-o", "--output", type=argparse.FileType('w'), default='-')
