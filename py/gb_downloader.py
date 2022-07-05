4cfd9030c49f405211f3e247516f32e6438d5fb3
markschl <ms201@gmx.net>
2022-07-05 14:04:53 +0200

    Refactor gb_downloader.py

--- py/gb_downloader.py
+++ py/gb_downloader.py
@@ -15,16 +15,23 @@ p = argparse.ArgumentParser(
                 "a file with Genbank identifiers or accessions (-i)",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
-p.add_argument("-i", "--ids", help="File with list of Genbank identifiers")
-p.add_argument("-s", "--idsep", default=",", help="ID file separator (if multiple columns there)")
-p.add_argument("-c", "--idcol", default=1, type=int, help="Column of file containing IDS")
-p.add_argument("--blast", action="store_true", help="BLAST output ids")
-p.add_argument("-q", "--query", help="Query ")
-p.add_argument("-e", "--email", required=True, help="required...")
-p.add_argument("-b", "--batch-size", type=int, default=20, help="batch size used to fetch data")
-p.add_argument("-d", "--database", default="nucleotide", help="")
-p.add_argument("-t", "--rettype", default="fasta", help="")
-p.add_argument("-n", "--num-tries", default=20, type=int,
+p.add_argument("-a", "--accessions", type=argparse.FileType('r'), 
+               help="File with list of Genbank accessions")
+p.add_argument("-s", "--acc-sep", default=",", 
+               help="accession file separator (if multiple columns present)")
+p.add_argument("-c", "--acc-col", default=1, type=int, 
+               help="Column of file containing accessions")
+p.add_argument("-q", "--query", 
+               help="Genbank Query to given database (nuccore by default, see -d)")
+p.add_argument("-e", "--email", required=True, 
+               help="E-mail address (required by Entrez service)")
+p.add_argument("-b", "--batch-size", type=int, default=100, 
+               help="batch size used to fetch data")
+p.add_argument("-d", "--database", default="nuccore", 
+               help="Database to query (default: nuccore)")
+p.add_argument("-t", "--rettype", default="fasta", choices={"fasta", "gb", "acc", "seqid"},
+               help="Return type (default: fasta); see https://www.ncbi.nlm.nih.gov/books/NBK25499/table/chapter4.T._valid_values_of__retmode_and")
+p.add_argument("-n", "--num-tries", default=100, type=int,
                help="Retry the given number of times before giving up if there is any error.")
 p.add_argument("-o", "--outfile", type=argparse.FileType('w'), default='-')
 args = p.parse_args()
@@ -34,7 +41,7 @@ Entrez.email = args.email
 
 
 class Loader(object):
-    def __init__(self, count, increment):
+    def __init__(self, increment, count=None):
         self.count = count
         self.increment = increment
         self.position = 0
@@ -46,10 +53,12 @@ class Loader(object):
         return self.next()
 
     def next(self):
-        if self.position >= self.count:
-            raise StopIteration()
-        write_progress(
-            f"downloading... {self.position:d} of {self.count:d} ({100 * self.position / self.count:.1f}%)")
+        if self.count is None:
+            write_progress(
+                f"downloading... {self.position}")
+        elif self.position <= self.count:
+            write_progress(
+                f"downloading... {self.position} of {self.count} ({100 * self.position / self.count:.1f}%)")
         data = self.fetch()
         self.position += self.increment
         return data
@@ -67,61 +76,64 @@ def write_progress(progress, target=sys.stderr):
     target.write(str(progress))
 
 
+class SearchLoader(Loader):
+    def __init__(self, db, term, increment):
+        handle = Entrez.esearch(db=db, term=term, usehistory="y")
+        self.props = Entrez.read(handle)
+        handle.close()
+        super(SearchLoader, self).__init__(increment=increment, count=int(self.props['Count']))
 
-if args.query:
-    handle = Entrez.esearch(db=args.database, term=args.query, usehistory="y")
-    props = Entrez.read(handle)
-    handle.close()
+    def fetch(self):
+        if self.position >= self.count:
+            raise StopIteration()
+        return Entrez.efetch(
+            db=args.database,
+            webenv=self.props['WebEnv'], query_key=self.props["QueryKey"],
+            rettype=args.rettype, retmode="text",
+            retstart=self.position,
+            retMax=args.batch_size
+        )
+ 
+
+class AccLoader(Loader):
+    def __init__(self, accessions, *arg, **kwarg):
+        super(AccLoader, self).__init__(*arg, **kwarg)
+        self.ids = None
+        self.iter = iter_chunks(accessions, self.increment)
+        
+    def get_id(self, id):
+        return id
     
-    class SearchLoader(Loader):
-        def fetch(self):
-            return Entrez.efetch(
-                db=args.database,
-                webenv=props['WebEnv'], query_key=props["QueryKey"],
-                rettype=args.rettype, retmode="text",
-                retstart=self.position,
-                retMax=args.batch_size
-            )
-            
-    loader = SearchLoader(int(props['Count']), args.batch_size)
-
-elif args.ids:
-    f = open(args.ids)
-    class IdLoader(Loader):
-        def __init__(self, *arg):
-            super(IdLoader, self).__init__(*arg)
-            self.ids = None
-            r = csv.reader(f, delimiter=args.idsep)
-            idcol = args.idcol - 1
-            self.iter = iter_chunks((self.get_id(row[idcol]) for row in r), self.increment)
-            
-        def get_id(self, id):
-            return id
-        
-        def next(self):
-            self.ids = list(next(self.iter))
-            return super(IdLoader, self).next()
-        
-        def fetch(self):
-            return Entrez.efetch(db=args.database, rettype=args.rettype, id=self.ids)
-    loader = IdLoader
-
-    if args.blast:
-        class BlastIdLoader(IdLoader):
-            def __init__(self, *arg):
-                super(BlastIdLoader, self).__init__(*arg)
-                self.pattern = re.compile(r"\w+\|(\d+)\|")
-
-            def get_id(self, line):
-                m = self.pattern.search(line)
-                return m.group(1)
+    def next(self):
+        self.ids = list(next(self.iter))
+        return super(AccLoader, self).next()
+    
+    def fetch(self):
+        return Entrez.efetch(db=args.database, rettype=args.rettype, id=self.ids)
 
 
-        loader = BlastIdLoader
+if args.query:    
+    loader = SearchLoader(db=args.database, term=args.query, increment=args.batch_size)
 
-    for i, line in enumerate(f): pass  # count lines
-    f.seek(0)
-    loader = loader(i + 1, args.batch_size)
+elif args.accessions:
+    f=args.accessions
+    # first, count lines
+    if f.seekable():
+        for i, line in enumerate(f): pass
+        f.seek(0)
+        count = i + 1
+    else:
+        count = None
+    # create accession iterator
+    r = csv.reader(f, delimiter=args.acc_sep)
+    c = args.acc_col - 1
+    accessions = (row[c].strip() for row in r)
+    accessions = (a for a in accessions if a)
+    loader = AccLoader(accessions, increment=args.batch_size, count=count)
+
+else:
+    raise Exception("Please supply one of '-q' or '-a'")
+
 
 import time
 
@@ -141,7 +153,8 @@ while True:
             out.write(data)
             break
         except StopIteration:
-            exit("\nDone\n")
+            print("\nDone\n", file=sys.stderr)
+            exit()
         except Exception as  e:
             retry_in = 5 * (i + 1)
             print(f"\n{e}: retrying after {retry_in} seconds\n", file=sys.stderr, flush=True)
